715d1b18f86b5a3a455c2000dac041996bf80b4c
Scott Beckstrøm <scott.beckstrom@geodata.no>
2021-12-01 10:10:07 +0100

    day 1 completed

--- day1/day1.py
+++ day1/day1.py
@@ -2,11 +2,36 @@ with open("inputs/day1.txt") as f:
     lines = f.readlines()
 lines = [int(line.rstrip()) for line in lines]
 
-increased: int = 0
 
-for i, n in enumerate(lines):
-    try:
-        if lines[i + 1] > n:
-            increased += 1
-    except IndexError:
-        print(f"{increased} increases.")
+def part1():
+    increased: int = 0
+
+    for i, n in enumerate(lines):
+        try:
+            if lines[i + 1] > n:
+                increased += 1
+        except IndexError:
+            print(f"{increased} increases for pt 1.")
+
+
+def part2():
+    increased: int = 0
+
+    for i, n in enumerate(lines):
+        try:
+            sum_a = n + lines[i + 1] + lines[i + 2]
+            sum_b = lines[i + 1] + lines[i + 2] + lines[i + 3]
+            if sum_b > sum_a:
+                increased += 1
+        except IndexError:
+            print(f"{increased} increases for pt 2.")
+            break
+
+
+def main():
+    part1()
+    part2()
+
+
+if __name__ == "__main__":
+    main()
