a83ca9ba92ec5adf2957b913e8cf3730b126f180
Scott Beckstrøm <scott.beckstrom@geodata.no>
2021-12-01 10:02:26 +0100

    day1 pt 1 complete!

--- day1/day1.py
+++ day1/day1.py
@@ -1,6 +1,12 @@
 with open("inputs/day1.txt") as f:
     lines = f.readlines()
+lines = [int(line.rstrip()) for line in lines]
 
-lines = [line.rstrip() for line in lines]
-for line in lines:
-    print (line)
+increased: int = 0
+
+for i, n in enumerate(lines):
+    try:
+        if lines[i + 1] > n:
+            increased += 1
+    except IndexError:
+        print(f"{increased} increases.")
