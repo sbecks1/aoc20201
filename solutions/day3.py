11e642fbc47800533db394fa7f577481c0135fd4
Scott Beckstrøm <scott.beckstrom@geodata.no>
2021-12-03 09:46:18 +0100

    day3, pt1

---
+++ solutions/day3.py
@@ -0,0 +1,25 @@
+with open("inputs/day3.txt") as f:
+    lines = [line.rstrip() for line in f.readlines()]
+
+
+def part1(input: "list[str]") -> int:
+    gamma = "0b"
+    epsilon = "0b"
+    for i in zip(*lines):
+        nums = [int(elem) for elem in i]
+        if sum(nums) > len(i) / 2:
+            gamma += "1"
+            epsilon += "0"
+        else:
+            gamma += "0"
+            epsilon += "1"
+
+    return int(gamma, 2) * int(epsilon, 2)
+
+
+def main():
+    print(part1(lines))
+
+
+if __name__ == "__main__":
+    main()
