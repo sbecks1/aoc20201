01f18f61108ffa96405bb9c682967d57a26b2ba6
Scott Beckstrøm <scott.beckstrom@geodata.no>
2021-12-03 11:34:29 +0100

    feels like an ugly solution to pt2

--- solutions/day3.py
+++ solutions/day3.py
@@ -17,11 +17,33 @@ def part1(input: "list[str]") -> int:
 
 
 def part2(input: "list[str]") -> int:
-    pass
+    def comm_char_at_bin_pos(input: "list[str]", bin_pos: int) -> str:
+        bin_positions = list(zip(*input))
+        if bin_positions[bin_pos].count("1") >= len(input) / 2:
+            return "1"
+        else:
+            return "0"
+
+    o2 = input.copy()
+    co2 = input.copy()
+    for i in range(len(input[0])):
+        if len(o2) > 1:
+            o2 = [n for n in o2 if n[i] == comm_char_at_bin_pos(o2, i)]
+        else:
+            break
+
+    for i in range(len(input[0])):
+        if len(co2) > 1:
+            co2 = [n for n in co2 if n[i] != comm_char_at_bin_pos(co2, i)]
+        else:
+            break
+
+    return int("0b" + o2[0], 2) * int("0b" + co2[0], 2)
 
 
 def main():
     print(part1(lines))
+    print(part2(lines))
 
 
 if __name__ == "__main__":
