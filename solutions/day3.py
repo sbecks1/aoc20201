c2d92a12718807ec2fd9e4c8bb55fa12382fa1e7
Scott Beckstrøm <scott.beckstrom@geodata.no>
2021-12-03 10:25:49 +0100

    probably should use function input

--- solutions/day3.py
+++ solutions/day3.py
@@ -5,7 +5,7 @@ with open("inputs/day3.txt") as f:
 def part1(input: "list[str]") -> int:
     gamma = "0b"
     epsilon = "0b"
-    for i in zip(*lines):
+    for i in zip(*input):
         if i.count("1") > len(i) / 2:
             gamma += "1"
             epsilon += "0"
