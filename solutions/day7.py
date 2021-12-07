035ab10dedf833f7c03a768258e4756264aa341a
scottb <scottabeckstrom@gmail.com>
2021-12-07 19:40:40 +0100

    day7, pt2. Pt2 runs sloooow. Probably should use actual math

--- solutions/day7.py
+++ solutions/day7.py
@@ -1,20 +1,33 @@
-from collections import Counter
 import math
 
 with open("inputs/day7.txt") as f:
-    data = f.read().rstrip().split(",")
+    data = [int(n) for n in f.read().rstrip().split(",")]
 
 
 def part1(data) -> int:
     min_fuel = math.inf
-    position_counts = Counter(data)
-    for org in position_counts:
+    for org in data:
         fuel_use = 0
-        for dest, val in position_counts.items():
-            fuel_use += abs(int(org) - int(dest)) * int(val)
+        for dest in data:
+            fuel_use += abs(org - dest)
         if fuel_use < min_fuel:
             min_fuel = fuel_use
     return min_fuel
 
 
+def part2(data) -> int:
+    min_fuel = math.inf
+    for org in range(min(data), max(data) + 1):
+        fuel_use = 0
+
+        for dest in data:
+            fuel_use += sum(range(abs(int(dest) - int(org)) + 1))
+
+        if fuel_use < min_fuel:
+            min_fuel = fuel_use
+
+    return min_fuel
+
+
 print(part1(data))
+print(part2(data))
